c15903000376522ad0f45642ae904fdbbe30cc22
FernLin <lynnxulink@gmail.com>
2020-10-22 00:33:59 +0800

    fix: 优化样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,19 +4,20 @@
 		<div class="content">
 			<div class="title">
 				<div>林棕梦ERP管理系统</div>
-				<div>欢迎登录</div>
+				<div style="margin-top: 20px">欢迎登录</div>
 			</div>
 			<el-input placeholder="请输入账号" prefix-icon="el-icon-user" v-model="account"> </el-input>
 			<el-input placeholder="请输入密码" prefix-icon="el-icon-lock" type="password" v-model="password"> </el-input>
-			<el-checkbox v-model="remember">记住密码</el-checkbox>
+			<!-- <el-checkbox v-model="remember">记住密码</el-checkbox> -->
 			<el-button type="primary" round @click="login">登录</el-button>
-			<div class="operate-content">
+			<!--当前系统不需要注册账号，所以将其隐藏，需要时打开即可-->
+			<!-- <div class="operate-content">
 				<span>
 					<span>没有账号？去</span>
 					<span class="link" @click="jumpTo('register')">注册</span>
 				</span>
 				<span class="link" @click="jumpTo('forget')">忘记密码？</span>
-			</div>
+			</div> -->
 		</div>
 	</div>
 </template>
@@ -52,13 +53,11 @@ export default {
 			}
 			const res = await UserLogin({ username: this.account, password: this.password });
 			if (!_.isEmpty(res)) {
-				if (this.remember) {
-					setLoginUserInfo({
-						res,
-						username: this.account,
-						password: this.password,
-					});
-				}
+				setLoginUserInfo({
+					res,
+					username: this.account,
+					password: this.password,
+				});
 				// 调用登录接口成功后跳转至系统主页面
 				this.$router.push({ path: '/FunctionList' });
 			}
@@ -81,7 +80,7 @@ export default {
 	}
 	.content {
 		width: 350px;
-		height: 350px;
+		height: 300px;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-around;
@@ -89,7 +88,7 @@ export default {
 		.title {
 			text-align: center;
 			font-size: 30px;
-			margin-bottom: 50px;
+			margin-bottom: 30px;
 		}
 		.operate-content {
 			display: flex;
