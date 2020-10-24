60b352ff3300a2398c814598f933dc1bbd49ef28
FernLin <lynnxulink@gmail.com>
2020-10-24 09:59:11 +0800

    fix: 格式化代码

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import store from './store/index';
 import './style/reset.scss';
 import './plugins/day';
-import "./plugins/element";
+import './plugins/element';
 import './plugins/sweetalert2';
 import _ from './utils/lodash';
 window._ = _;
@@ -12,7 +12,7 @@ window._ = _;
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount('#app');
